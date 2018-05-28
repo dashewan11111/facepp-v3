2264229292dc770f6bb2d59a23d1272f480d0eb9
licheng <licheng@megvii.com>
2018-05-28 11:05:55 +0800

    [init] 初始化工程

---
+++ faceppdemo/src/main/java/com/facepp/demo/util/Util.java
@@ -0,0 +1,11 @@
+package com.facepp.demo.util;
+
+public class Util {
+
+	//在这边填写 API_KEY 和 API_SECRET
+	public static String API_KEY = "sb8yuIxw8WP4mIFIXrZmgJElJe1BDomc";
+	public static String API_SECRET = "SAmCvobwwv-Qs09Sme0XxH7nt7Fqq9Yf";
+
+	public static String CN_LICENSE_URL = "https://api-cn.faceplusplus.com/sdk/v3/auth";
+	public static String US_LICENSE_URL = "https://api-us.faceplusplus.com/sdk/v3/auth";
+}
